6eb3a9492d835d52d43214ec7f6ed83f8d168231
mxptx22 <104283629+mxptx22@users.noreply.github.com>
2022-06-21 23:12:34 +0100

    updated code

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -12,7 +12,7 @@ function Courses() {
     const {screen, setScreen} = useContext(PageContext)
     const {courses, addCourses} = useContext(PageContext)
     const {selectedCourse, setSelectedCourse} = useContext(PageContext)
-    const {savingStatus,  setSavingStatus} = useContext(PageContext)
+    const { savingStatus, setSavingStatus } = useContext(PageContext)
     
 
 
@@ -22,46 +22,64 @@ function Courses() {
             coursesCongifBar.current.style.backgroundColor = 'lightseagreen'
             setSavingStatus('yas')
             determineSaving('yas')
+            console.log('i ran saving for later')
         } 
         else {
             setSavingSliderDefault('start')
             coursesCongifBar.current.style.backgroundColor = 'gray'
             setSavingStatus('nay')
             localStorage.clear()    
+            console.log('i abstained from saving for later')
         }
     }
     
     function determineSaving(switcher) {
-        if (switcher === 'yas') {localStorage.setItem('saving', 'yas')}
+        if (switcher === 'yas') { localStorage.setItem('saving', 'yas') }
+        if (switcher === 'yas' && courses.length > 0) {saveCourse()}
         if (switcher === 'nay') {localStorage.setItem('saving', 'nay')}
     }
 
     function processSaving() {
-        if (localStorage.getItem('saving') === 'yas') {
-            setSavingStatus('yas')
+        if (localStorage.getItem('saving') !== 'yas') {
+            console.log('no course shall pass')
+        }
+        else {
             savingforLater()
+            console.log('i ran process saving')
             const retrievedCourses = JSON.parse(localStorage.getItem('savedCourses'));
-            if (retrievedCourses) {
-             addCourses(retrievedCourses);
+
+            if (retrievedCourses !== null && courses.length === 0) {
+                addCourses(retrievedCourses);
+                console.log('i even ran adding courses from saved courses in localStorage')
             }
         }
     }
 
     function saveCourse() {
         localStorage.setItem('savedCourses', JSON.stringify(courses))
+         console.log('i saved a course with savecourse')
     }
 
     useEffect(() => {
         processSaving()
+         console.log('processSaving was run on Effect')
     
     }, [])
 
-    function refreshPage() {
-    window.location.reload(false);
-  }
+
     
     useEffect(() => {
-        if (courses.length !== 0) { saveCourse()}
+        if (courses.length !== 0) {
+            if (savingStatus !== 'yas') {
+                console.log('there will be no saving with saveCourse')
+                localStorage.clear() 
+                // here is new
+            } else {
+                console.log('since you want saving, I added and ran savecourse into localStorage')
+                saveCourse();
+            }
+        }
+
     
     }, [courses])
     
